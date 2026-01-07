a6c76edbae06f25f5b0cd104ff4ed480a737619d
Fael <devrafaelcarvalho14@gmail.com>
2026-01-07 16:11:17 -0300

    feat: add inventory new dependencies

--- src/apps/inventory/inventory.ts
+++ src/apps/inventory/inventory.ts
@@ -1,10 +1,16 @@
 import { db } from "../../infra/db/postgres/postgres";
 import { InventoryRepository } from "./repository/inventory.repository";
 import { InventoryService } from "./service/inventory.service";
+import { InventoryWorker } from "./worker/inventory.worker";
 
 export const inventory = {
   createInventoryService: () => {
     const inventoryRepository = new InventoryRepository(db);
     return new InventoryService(inventoryRepository);
+  },
+
+  createInventoryWorker: () => {
+    const inventoryService = inventory.createInventoryService();
+    return new InventoryWorker(inventoryService);
   },
 };
