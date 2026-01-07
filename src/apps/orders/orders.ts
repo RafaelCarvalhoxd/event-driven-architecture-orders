e3288953d9c844a4928d4b507f678ed81ca6c2fd
Fael <devrafaelcarvalho14@gmail.com>
2026-01-07 16:10:46 -0300

    feat: add rabbitmq to order

--- src/apps/orders/orders.ts
+++ src/apps/orders/orders.ts
@@ -1,12 +1,18 @@
 import { db } from "../../infra/db/postgres/postgres";
+import { RabbitMQPublisher } from "../../infra/messaging/rabbitmq/publisher";
 import { OrdersRepository } from "./repository/orders.repository";
 import { OrdersService } from "./service/orders.service";
+import { OrdersWorker } from "./worker/orders.worker";
 
 export const orders = {
   createOrdersService: () => {
     const ordersRepository = new OrdersRepository(db);
-    return new OrdersService(ordersRepository);
+    const rabbitMQPublisher = new RabbitMQPublisher();
+    return new OrdersService(ordersRepository, rabbitMQPublisher);
+  },
+
+  createOrdersWorker: () => {
+    const ordersService = orders.createOrdersService();
+    return new OrdersWorker(ordersService);
   },
 };
-
-
